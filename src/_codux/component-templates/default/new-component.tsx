c9a51af3c324f52efdfff6441693e54d042128cf
Ari Kishon <37385467+Ari-Kishon@users.noreply.github.com>
2023-06-20 15:14:01 +0300

    fix: KB article in component template

--- src/_codux/component-templates/default/new-component.tsx
+++ src/_codux/component-templates/default/new-component.tsx
@@ -8,7 +8,7 @@ export interface NewComponentProps {
 
 /**
  * This component was generated using Codux's built-in Default new component template.
- * For details on how to create custom new component templates, see https://help.codux.com/kb/en/article/configuration-for-new-components-and-templates
+ * For details on how to create custom new component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const NewComponent = ({ className, children = 'NewComponent' }: NewComponentProps) => {
     return <div className={classNames(styles.root, className)}>{children}</div>;
